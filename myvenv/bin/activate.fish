e259c9ad0e95027cf36bff0e34b11930bf99551d
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2020-12-03 08:56:59 +0900

    myvenv setting

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/hondaatsuya/djangogirls/myvenv"
+set -gx VIRTUAL_ENV "/Users/hondaatsuya/Documents/djangoblog/DjangoBlog/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
